23f5bafed49642953ee7b56931ba05247fc58257
大和田健大 <kenta-o@weseek.co.jp>
2020-10-31 20:48:02 +0900

    トップページの大枠を作成

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,7 +5,6 @@ import '../styles/global.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
-
   return (
     <Component {...pageProps} />
   );
---
+++ pages/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+function Top() {
+  return(
+    <>
+      <h2 className="text-center">好きなお菓子をランク付けしよう</h2>
+      <div className="container my-5 py-5 bg-success">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="px-3 bg-info">
+              <div className="sweet-item">
+                <p className="pt-3">kitcat</p>
+                <div className="sweet-review">
+                  最高にうまい
+                </div>
+                <img src="/static/images/star.svg" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="px-3 bg-info">
+              <div className="sweet-item">
+                <p className="pt-3">ブラックサンダー</p>
+                <div className="sweet-review">
+                  ブラックサンダー
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="px-3 bg-info">
+              <div className="sweet-item">
+                <p className="pt-3">チョコボール</p>
+                <div className="sweet-review">
+                  チョコボール
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            お菓子レポーターランキング
+          </div>
+          <div className="col-md-6">
+            お菓子ランキング
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Top;
