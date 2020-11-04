cb18471c7285172cfc87f110e2980de6bca3336b
yusuketk <yusuketk@weseek.co.jp>
2020-11-04 21:34:19 +0900

    trap 2 fails for test ci

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import PropTypes from 'prop-types';
 
 import '../styles/global.scss';
-import { AppProps } from 'next/app';
+import { AppProps } from 'next/app'
 import Navbar from '../components/navbar';
 
 // This default export is required in a new `pages/_app.js` file.
