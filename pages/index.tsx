19001f5758e5ebcbbca18947c3b8726af9bcafc4
大和田健大 <kenta-o@weseek.co.jp>
2020-11-07 20:20:33 +0900

    bootstrap の名残を消去

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,15 +7,15 @@ const sweetReviews = [...Array(3)].map((value, i) => {
   return (
     // 後でkeyの名前はお菓子の名前にする。
     // eslint-disable-next-line react/no-array-index-key
-    <div className="col-md-4" key={i}>
-      <div className="px-3 bg-light">
+    <div key={i}>
+      <div>
         <div className="sweet-item">
-          <p className="pt-3">ブラックサンダー</p>
+          <p>ブラックサンダー</p>
           <div className="sweet-review">
             まあまあうまい
           </div>
         </div>
-        <div className="py-3">
+        <div>
           <Star />
           <Star />
         </div>
@@ -30,28 +30,20 @@ const sweets: Array<string> = ['チュッパチャップス', 'kitkat', 'ぼた�
 const Top: React.FC = () => {
   return (
     <>
-      <h2 className="text-center my-4">好きなお菓子をランク付けしよう</h2>
+      <h2>好きなお菓子をランク付けしよう</h2>
       <Container maxWidth="md">
-        <div className="container my-5 py-5 bg-success">
-          <div className="row">
-            {sweetReviews}
+        <p>{sweetReviews}</p>
+        <div>
+          <div>
+            <h3>
+              お菓子レポーターランキング
+            </h3>
           </div>
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 bg-success">
-              <div className="row">
-                <h3>
-                  お菓子レポーターランキング
-                </h3>
-              </div>
-            </div>
-            <div className="col-md-6 bg-success">
-              <h3>
-                お菓子ランキング
-              </h3>
-            </div>
-          </div>
+        <div>
+          <h3>
+            お菓子ランキング
+          </h3>
         </div>
         <NewReviewForm
           sweets={sweets}
