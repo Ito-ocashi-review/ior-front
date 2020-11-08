2a9fffa723f4d419ab5a6a3ebebb2c313770167b
zahmis <meda829ratteru86@gmail.com>
2020-11-08 16:45:56 +0900

    add controller

--- components/forms/ReviewText.tsx
+++ components/forms/ReviewText.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
 import { TextField } from '@material-ui/core';
+import React from 'react';
+import {useForm, Controller} from 'react-hook-form';
 
 const ReviewText: React.FC = () => {
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = data => {
+    console.log(data)
+  };
+
   return (
-    <div>
-      <TextField
-        id="standard-textarea"
-        label="コメントを投稿"
-        placeholder="投稿内容を入力"
-        fullWidth
-        rows="5"
-        multiline
-        margin="normal"
-      />
-    </div>
+    
+        <Controller
+          name="sweetReview"
+          control={control}
+          render={({ onChange, value }) => (
+            <TextField
+              id="standard-textarea"
+              label="コメントを投稿"
+              placeholder="投稿内容を入力"
+              fullWidth
+              rows="5"
+              multiline
+              margin="normal"
+            />
+          )}
+          >
+         </Controller>
   );
 };
 
