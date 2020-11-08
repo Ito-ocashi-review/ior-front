a91be26951e6258132789d5d68804eb094f9ecf8
zahmis <meda829ratteru86@gmail.com>
2020-11-08 18:25:05 +0900

    useFormContext

--- components/forms/ReviewText.tsx
+++ components/forms/ReviewText.tsx
@@ -1,32 +1,31 @@
-import { TextField } from '@material-ui/core';
 import React from 'react';
-import {useForm, Controller} from 'react-hook-form';
+import { useForm, Controller, useFormContext } from 'react-hook-form';
+import { TextField } from '@material-ui/core';
+
 
 const ReviewText: React.FC = () => {
-  const { handleSubmit, control } = useForm();
-
-  const onSubmit = data => {
-    console.log(data)
-  };
+  const { control } = useFormContext();
 
   return (
-    
-        <Controller
-          name="sweetReview"
-          control={control}
-          render={({ onChange, value }) => (
-            <TextField
-              id="standard-textarea"
-              label="コメントを投稿"
-              placeholder="投稿内容を入力"
-              fullWidth
-              rows="5"
-              multiline
-              margin="normal"
-            />
-          )}
-          >
-         </Controller>
+    <>
+      <Controller
+        name="sweetReview"
+        control={control}
+        render={({ onChange, value }) => (
+          <TextField
+            value={value}
+            id="standard-textarea"
+            label="コメントを投稿"
+            placeholder="投稿内容を入力"
+            fullWidth
+            rows="5"
+            multiline
+            margin="normal"
+            onChange={onChange}
+          />
+        )}
+      />
+    </>
   );
 };
 
