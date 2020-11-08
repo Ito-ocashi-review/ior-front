09e7fb44b053aef4a3ad5a856937eca39fa95d8e
大和田健大 <kenta-o@weseek.co.jp>
2020-11-08 15:18:32 +0900

    rating が投稿できるようになった

--- components/forms/EvaluetionForm.tsx
+++ components/forms/EvaluetionForm.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
 import { Rating } from '@material-ui/lab';
 import { Typography } from '@material-ui/core';
 
 const EvaluationForm: React.FC = () => {
-  const [value, setValue] = React.useState<number | null>(2);
+  const { control, watch } = useFormContext();
+  const rating: number = watch('rating', 0);
+
   return (
     <>
       <Typography component="legend">評価する</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        precision={0.1}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
+      <Controller
+        control={control}
+        name="rating"
+        render={({ onChange, value }) => (
+          <Rating
+            value={value}
+            precision={0.1}
+            onChange={onChange}
+          />
+        )}
+      >
+      </Controller>
+      <p>{rating}</p>
     </>
   );
 };
