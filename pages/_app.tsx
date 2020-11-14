a37bc0dc644689abb11b8f81ab7360ee1c5b434b
大和田健大 <kenta-o@weseek.co.jp>
2020-11-14 20:04:31 +0900

    reset default css setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,14 @@ import PropTypes from 'prop-types';
 import '../styles/global.scss';
 import { AppProps } from 'next/app';
 import Navbar from '../components/navbar';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 // This default export is required in a new `pages/_app.js` file.
 const App = ({ Component, pageProps }:AppProps):ReactElement => {
   return (
     <>
+      {/* reset default css setting */}
+      <CssBaseline />
       <Navbar />
       <Component {...pageProps} />
     </>
