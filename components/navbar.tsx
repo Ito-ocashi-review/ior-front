0eda3b6df66f9d1d58639b165c56d5df46970f40
大和田健大 <kenta-o@weseek.co.jp>
2020-11-15 13:41:21 +0900

    - sweetalert 関連のライブラリをインストール - モーダルにreact element を埋め込めるようになった

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+import ReactDOMServer from 'react-dom/server';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import {
   AppBar, Toolbar, Typography, IconButton, Button,
 } from '@material-ui/core';
@@ -17,8 +21,27 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const renderLoginHtml = () => {
+  return ReactDOMServer.renderToStaticMarkup(
+    <div>
+      <GitHubIcon />
+      <p>githubログインする</p>
+    </div>
+    ,
+  );
+};
+
 const MenuAppBar:React.FC = () => {
   const classes = useStyles();
+
+  const MySwal = withReactContent(Swal);
+
+  const handleLogin = async() => {
+    await MySwal.fire({
+      title: 'ログインする',
+      html: renderLoginHtml(),
+    });
+  };
 
   return (
     <AppBar position="static">
@@ -29,6 +52,9 @@ const MenuAppBar:React.FC = () => {
         <Typography variant="h6" className={classes.title}>
           いとおかし
         </Typography>
+        <Button onClick={handleLogin}>
+          ログイン
+        </Button>
       </Toolbar>
     </AppBar>
   );
