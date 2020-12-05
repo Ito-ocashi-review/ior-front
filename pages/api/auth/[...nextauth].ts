3fcb367ab161410d512c48fa026414314a62776c
itizawa <itizawa@weseek.co.jp>
2020-12-05 14:26:03 +0900

    sign in github

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 const options = {
-  site: 'http://localhost:3000',
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,7 +12,6 @@ const options = {
   // A database is optional, but required to persist accounts in a database
   database: process.env.MONGO_URI || 'mongodb://mongo:27017/ior-back',
 
-  jwt: true,
   sessionMaxAge: 30 * 24 * 60 * 60 * 1000, // Expire sessions after 30 days of being idle
   sessionUpdateAge: 24 * 60 * 60 * 1000, // Update session expiry only if session was updated more recently than the last 24 hours
 };
