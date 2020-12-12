b57672278b3cd1096ba3708b8f6b2acbcc882934
Yokohama-shinagawa <kenta2231989@gmail.com>
2020-12-12 23:28:17 +0900

    twitter認証を追記

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,6 +7,10 @@ const options = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+    }),
   ],
 
   // A database is optional, but required to persist accounts in a database
