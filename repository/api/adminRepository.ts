d0e0a1b789b16d66d9d9c2baf71bb7baca9f3b7a
zahmis <meda829ratteru86@gmail.com>
2020-12-20 20:41:10 +0900

    fix: type

--- repository/api/adminRepository.ts
+++ repository/api/adminRepository.ts
@@ -4,7 +4,7 @@ type data = {
   sweet: string,
 }
 
-export const postSweet = (data:data):Promise<any> => {
+export const postSweet = (data:data):Promise<typeof axiosInstance> => {
   return axiosInstance.post('/api/sweets', {
     name: data.sweet,
   });
