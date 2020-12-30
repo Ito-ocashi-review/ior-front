640ae3bf827cf565e0a749cd7457dc927105dc6d
kntowd <kenta2231989@gmail.com>
2020-12-30 16:34:44 +0900

    rankingの順位の表示部分をコンポーネント化

---
+++ components/topRanking/Ranking.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  ranking: {
+    fontFamily: 'Luckiest Guy',
+    fontSize: '80px',
+    fonttWeight: '80px',
+    letterSpacing: '8.9px',
+    WebkitTextStroke: '4px #000',
+  },
+  one: {
+    color: '#F7CB00',
+  },
+  two: {
+    color: '#D3D3D3',
+  },
+  three: {
+    color: '#C47222',
+  },
+}));
+
+const Ranking:React.FC = ({ number }) => {
+  const classes = useStyles();
+
+  const rankingDisplay = (num) => {
+    if (num === 1) {
+      return (
+        <span className={`${classes.ranking} ${classes.one}`}>No.1</span>
+      );
+    }
+    if (num === 2) {
+      return (
+        <span className={`${classes.ranking} ${classes.two}`}>No.2</span>
+      );
+    }
+    return <span className={`${classes.ranking} ${classes.three}`}>No.3</span>;
+  };
+
+  return (
+    rankingDisplay(number)
+  );
+};
+
+export default Ranking;
--- components/topRanking/SweetRanking.tsx
+++ components/topRanking/SweetRanking.tsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import {
-  Grid, makeStyles,
+  Grid,
 } from '@material-ui/core';
 import SweetCard from './SweetCard';
-
-const useStyles = makeStyles(theme => ({
-  ranking: {
-    fontFamily: 'Luckiest Guy',
-    fontSize: '80px',
-    fonttWeight: '80px',
-    color: '#F7CB00',
-    letterSpacing: '8.9px',
-    WebkitTextStroke: '2px #000',
-  },
-}));
+import Ranking from './Ranking';
 
 const SweetRanking:React.FC = () => {
-  const classes = useStyles();
   const filePaths = ['/image/jagariko.png', '/image/poteti.png', '/image/umaibou.png'];
-
   const cards = filePaths.map((filePath, index) => {
     return (
       <Grid item xs={4} key={filePath}>
-        <span className={classes.ranking}>No.{index + 1}</span>
+        <Ranking
+          number={index + 1}
+        />
         <SweetCard
           filePath={filePath}
         />
