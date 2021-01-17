281bc1051391405363e0b482edbf06f155bf0f9c
大和田健太 <53945259+kntowd@users.noreply.github.com>
2021-01-17 13:42:35 +0900

    Revert "Create add sweet feature"

--- repository/api/sweetsRepository.ts
+++ repository/api/sweetsRepository.ts
@@ -1,15 +1,5 @@
 import { axiosInstance } from '../axios';
 
-type data = {
-  sweet: string,
-}
-
-export const getSweet = ():void => {
-  axiosInstance.get('/api/sweets');
-};
-
-export const createSweet = (data:data):void => {
-  axiosInstance.post('/api/sweets/', {
-    name: data.sweet,
-  });
+export const getSweet = ():Promise<any> => {
+  return axiosInstance.get('/api/sweets');
 };
